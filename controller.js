9f3576ded1c175930dd5d89af904eda44f8279ce
Max <maryasych@gmail.com>
2018-10-09 10:20:53 +0300

    gitignore fail

---
+++ controller.js
@@ -0,0 +1,67 @@
+const logic = require('./logic'),
+    Datastore = require('nedb'),
+    db = new Datastore({
+        filename: 'dd',
+        autoload: true
+    });
+db.loadDatabase(function (err) { // Callback is optional
+    // Now commands will be executed
+});
+
+let buttons = document.querySelectorAll('#buttons button:not(:last-child)'),
+    clear = document.getElementById('clear'),
+    searchField = document.getElementById('search-field'),
+    scrapped = document.getElementById('scrapped'),
+    proc = document.getElementById('process'),
+    info = document.getElementById('info'),
+    currency = 1;
+
+document.getElementById('currency').onblur = function () {
+    currency = isNaN(this.value) ?  this.value = 1 : this.value;
+};
+
+[...buttons].forEach(item => {
+    item.addEventListener('click', function () {
+        logic.count(this.id)
+    })
+})
+clear.onclick = () => db.remove({}, {
+        multi: true,
+    })
+searchField.oninput = function () {
+    if (this.value.length >= 3) {
+        let val = this.value;
+        db.find({
+            prodName: new RegExp(val, 'i')
+        }).sort({
+            company: 1,
+            stock: 1,
+            price: 1,
+        }).exec(
+            function (err, res) {
+                if (err) {
+                    console.log(err);
+                    return 0;
+                }
+                showTable(res, scrapped)
+            })
+    } else if (this.value.length == 0) {
+        scrapped.innerHTML = ''
+    }
+};
+
+function showTable(res, result) {
+    result.innerHTML = '';
+    res.forEach((item, i) => {
+        result.innerHTML += `
+        <tr>
+        <td>${res[i].company}</td>
+        <td>${res[i].prodName}</td>
+        <td>${((res[i].price)/currency).toFixed(2)}</td>
+        ${res[i].stock.toLowerCase().replace(/\s+/g, "") == 'нетвналичии' || 
+        res[i].stock.toLowerCase().replace(/\s+/g, "") == 'outofstock' || 
+        res[i].stock.toLowerCase().replace(/\s+/g, "") == 'ожидается' ?
+        `<td class="bg-info">${res[i].stock}</td>` : `<td>${res[i].stock}</td>`}
+        </tr>`
+    })
+}
---
+++ logic.js
@@ -0,0 +1,67 @@
+const DomParser = require('dom-parser'),
+  needle = require('needle'),
+  parser = new DomParser(),
+  pattern = require('./module');
+  company = pattern.pattern.company,
+  stopper = false;
+
+exports.count = async function count(category) {
+  for (let url_key in company) {
+    let url = company[url_key].url[category]
+    await say(url, url_key, company[url_key].url_end)
+  }
+}
+
+function delay() {
+  return new Promise(resolve => setTimeout(resolve, 3000), reject => console.log(new Error))
+}
+async function say(url, url_key, url_end) {
+  //await delay()
+  await steps(url, url_key, url_end)
+}
+async function steps(url, url_key, url_end) {
+  let i = 1;
+  for (i; i < 25; i++) {
+    info.innerHTML = `Scanned ${i-1} pages in ${url_key} company`;
+    if (stopper == true) {
+      stopper = false;
+      return Promise.resolve()
+    };
+    await delay();
+    //console.log(`url = ${url} url_key = ${url_key} url_end = ${url_end}`)
+    i == 1 ? await getContent(url, url_key) : await getContent(url + url_end + i + '/', url_key)
+    //console.log(url + url_end + i + '/', url_key, `i = ${i}`)
+  }
+}
+async function getContent(productCategory, company_key) {
+  var company = pattern.pattern.company[company_key]
+  console.log(productCategory)
+  await needle('get', productCategory)
+    .then(res => parser.parseFromString(res.body))
+    .then(dom => company.getProductList(dom))
+    .then(productList => {
+      productList.forEach(function (item) {
+        if (item.nodeName == company.identifier) {
+          var productCard = company.getProductCard(item);
+          var productProperites = company.getProductProperites(productCard)
+          db.insert({
+              "company": company.name,
+              "prodName": productProperites.productName,
+              "price": productProperites.productPrice.match(/\d+.\d+/g)[0].replace(',','.'),
+              "stock": productProperites.stockStatus
+            })
+        }
+      })
+    })
+    .then(db.find({}, (err,res)=>{
+      if(err){
+        alert(err)
+        return Promise.resolve();
+      }
+      proc.innerHTML = `Has been scanned ${res.length} products`
+    }))
+    .catch(e => {
+      alert(e)
+      stopper = true;
+    })
+}
